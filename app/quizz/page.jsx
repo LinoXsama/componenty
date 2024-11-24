ce33aa4d79917d476eea9f56483db3aac270ee06
Merlin Migan <slavikzaya@gmail.com>
2024-11-24 22:52:13 +0100

    Ajout de la possibilité de sélectionner les réponses du quizz !

--- app/quizz/page.jsx
+++ app/quizz/page.jsx
@@ -1,26 +1,91 @@
 'use client';
 
-import { useEffect, useState } from 'react'
-import { Button } from '@/components/ui/button'
-import { Progress } from '@/components/ui/progress'
+import { useEffect, useState } from 'react';
+
+import { Button } from '@/components/ui/button';
+import { Progress } from '@/components/ui/progress';
+import { SlashIcon } from "@radix-ui/react-icons";
+import {
+   Breadcrumb,
+   BreadcrumbItem,
+   BreadcrumbLink,
+   BreadcrumbList,
+   BreadcrumbPage,
+   BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
+
+import Image from 'next/image';
 
 const Quizz = () => {
-
    const [progress, setProgress] = useState(13);
+   const [selectedImage, setSelectedImage] = useState(null); // État pour l'image sélectionnée
 
    useEffect(() => {
       const timer = setTimeout(() => setProgress(66), 500);
 
       return () => clearTimeout(timer);
-   });
+   }, []);
+
+   // Fonction pour gérer la sélection d'image
+   const handleImageClick = (imageId) => {
+
+      if(selectedImage === imageId) {
+         setSelectedImage(null);
+      } else {
+         setSelectedImage(imageId);
+      }
+      console.log(selectedImage)
+   };
 
    return (
       <>
+         <div className='border border-blue-500 w-full h-screen flex flex-col justify-center items-center'>
+            <div className='border border-green-500 w-[50%] mb-8'>
+               <h1 className='uppercase border mb-3 text-cyan-500 text-xl font-bold'>
+                  question 1
+               </h1>
+               <p className='border text-lg'>
+                  Parmi ces 3 images, laquelles correspond le plus au besoin
+                  évoqués précédemment ?
+               </p>
+            </div>
+            <div className='border border-purple-500 w-[50%] flex justify-between items-center'>
+               <Image
+                  id="rocketlaunch"
+                  src="/images/rocketlaunch.png"
+                  width={200}
+                  height={200}
+                  className={`border cursor-pointer ${selectedImage === 1 ? 'border-4 border-blue-600' : ''}`}
+                  onClick={() => handleImageClick(1)}
+               />
+               <Image
+                  id="rocketman"
+                  src="/images/rocketman.png"
+                  width={200}
+                  height={200}
+                  className={`border cursor-pointer ${selectedImage === 2 ? 'border-4 border-blue-600' : ''}`}
+                  onClick={() => handleImageClick(2)}
+               />
+               <Image
+                  id="rocketride"
+                  src="/images/rocketride.png"
+                  width={200}
+                  height={200}
+                  className={`border cursor-pointer ${selectedImage === 3 ? 'border-4 border-blue-600' : ''}`}
+                  onClick={() => handleImageClick(3)}
+               />
+            </div>
+         </div>
+
          <div className="border border-red-500 absolute bottom-0 w-full flex justify-between px-4 py-2 items-center">
-            <Progress value={progress} className="w-[18%]" />
-            <Progress value={progress} className="w-[18%]" />
-            <Button className="uppercase">
-               next question
+            <Button className="uppercase cursor-pointer">
+               question précédente
+            </Button>
+           
+            <Progress value={progress} className="w-[25%]" />
+
+            <Button className="uppercase cursor-pointer">
+               question suivante
             </Button>
          </div>
       </>
